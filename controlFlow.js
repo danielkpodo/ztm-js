eb54fef704def74dd8e23bd2f9b60386b19540a6
danielkpodo <kpododanielnarh@gmail.com>
2021-02-21 12:59:51 +0000

    added control flow

--- controlFlow.js
+++ controlFlow.js
@@ -3,3 +3,5 @@ var user = prompt("What is your name? ");
 if (user === "naphtha") {
   console.log("Nice, your guess is right");
 }
+
+//Reference error means js does not understand the value type of a variable
