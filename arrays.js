65b539e84491d9dba2f5f73a5912575a64652e6e
danielkpodo <kpododanielnarh@gmail.com>
2021-03-24 13:06:24 +0000

    added arrays

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
+/*
+
+Arrays are useful data-strutures for organizing things
+useful array methods: concat, sort,
+
+Utilize more of w3schools references for arrays and general js
+
+*/
+
 const animals = ["Tiger", "Lion", "Chameleon", "Hyena", "Lizard"];
 
 const wildAnimals = animals.slice(1, 3);
