7a00c8f40df43f95c7ac068c59ce858a4bedde19
danielkpodo <kpododanielnarh@gmail.com>
2021-03-24 07:54:05 +0000

    added a simple function test

--- functions.js
+++ functions.js
@@ -1,4 +1,23 @@
 /* Functions allows us to make our code re-usable
 It allows us to avoid duplication in our code
 A function that does not return a value spits out undefined
+
+For abstraction purposes make sure your func returns true
 */
+
+function checkDriverAge(age) {
+  if (!validInput(age)) {
+    return console.log("Please enter a number");
+  }
+  if (age < 18) return console.log("Too young to drive");
+  if (age > 18) return console.log("Powering on, drive fast");
+  if (age === 18) return console.log("Congratulations on your first drive");
+}
+
+function validInput(value) {
+  if (typeof value === "number") return true;
+
+  return false;
+}
+
+checkDriverAge(27);
