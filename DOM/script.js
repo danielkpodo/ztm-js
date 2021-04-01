a66474e139598f76a7eff6d35f5ddf5c34a5a2d3
danielkpodo <kpododanielnarh@gmail.com>
2021-04-01 12:28:31 +0000

    added how to get and set attribtutes

--- DOM/script.js
+++ DOM/script.js
@@ -6,3 +6,22 @@ console.log(content);
 
 //2. getElementsByTagName - array
 //3. getElementById - singular
+
+//Recommended selectors
+//.1 document.querySelector - singular
+//2. document.querySelctorAll - plural
+
+//Setting and getting attributes
+//1. Get attribute
+// const initialDiv = document.querySelector("div");
+// let initialDivAttr = initialDiv.getAttribute("class");
+// console.log(initialDivAttr);
+
+//2. Set attribute
+//document.setAttribute("content", "wooow");
+
+// const li = document.querySelector("li");
+// console.log(li);
+// const random = li.getAttribute("random");
+// // console.log(random);
+// li.setAttribute("random", "90000000");
