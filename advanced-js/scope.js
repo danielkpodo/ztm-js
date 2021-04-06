9fc47c649d89e040cf4af565e102d53fb606be70
danielkpodo <kpododanielnarh@gmail.com>
2021-04-06 03:52:17 +0000

    added scope

---
+++ advanced-js/scope.js
@@ -0,0 +1,4 @@
+//Scope means what variables do I have access to?
+//Functions have access to variables within the root scope  or window scope
+
+//when you run a function it always checks the parent scope or global scope if a variable named in its child scope exists there
