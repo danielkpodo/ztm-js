ad5592209817ea675ab9be0d844aef2c53a9f623
danielkpodo <kpododanielnarh@gmail.com>
2021-04-07 22:39:40 +0000

    introduced let & const

--- advanced-js/es5-es6.js
+++ advanced-js/es5-es6.js
@@ -1,1 +1,8 @@
 //Babel allows us to convert new js features to forms that are compatible with all browsers
+//let & const
+/* 
+let can be used for reassigning variables
+*Let is also a block scope variable -> We say it is block scope
+const is used for constant variables
+NB:// With objects that are decaled using constants you can change the properties
+*/
